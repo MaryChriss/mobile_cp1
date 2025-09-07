67a01a58ec197b411bf31f90a6eb97675de0d405
Mariana Christina <161539215+MaryChriss@users.noreply.github.com>
2025-09-07 19:39:42 -0300

    clean code

--- src/services/characterService.ts
+++ src/services/characterService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = "https://www.demonslayer-api.com/api/v1/characters?limit=45";
 
-export const getCharacters = async (p0: string | number) => {
+export const getCharacters = async () => {
   const response = await axios.get(API_URL);
   console.log(response.data);
   return response.data.content;
